9754f748104390667a301827ea02003a769946f1
Gianpietro Lavado <gianpietro1@gmail.com>
2019-01-09 03:47:33 +0000

    prepare API

--- 00_prepare_api.py
+++ 00_prepare_api.py
@@ -8,7 +8,7 @@ for host in ip_list:
   cisco_ios_xrv = {'device_type': 'cisco_xr', 'ip': host, 'username': 'ws', 'password': 'ws'}
   net_connect = ConnectHandler(**cisco_ios_xrv)
 
-  config = [i'xml agent tty iteration off','netconf-yang agent ssh','ssh server netconf vrf default','commit']
+  config = ['xml agent tty iteration off','netconf-yang agent ssh','ssh server netconf vrf default','commit']
 
   output = net_connect.send_config_set(config)
   print(output)
